5892371f23ecd3ce4816740fb278bc3b88f33b5a
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2023-08-09 17:59:09 +0300

    sovle eslint

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
-const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
@@ -64,7 +63,6 @@ app.use((req, res, next) => {
 });
 
 // 3) ROUTES
-app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
--- server.js
+++ server.js
@@ -10,10 +10,15 @@ process.on('uncaughtException', err => {
 dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD
-);
+let DB;
+if (process.env.NODE_ENV === 'production') {
+  DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+  );
+} else {
+  DB = process.env.DATABASE_LOCAL;
+}
 
 mongoose
   .connect(DB, {
