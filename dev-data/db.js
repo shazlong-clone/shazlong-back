40f85c84c3b1072e40eaa7cb48fdb5891315910b
saeedkhal <saeedx.saeed99@eng-st.cu.edu.eg>
2024-12-25 14:08:58 +0200

    update doctor dumy data

--- dev-data/db.js
+++ dev-data/db.js
@@ -9,10 +9,12 @@ if (process.env.NODE_ENV === 'production') {
   console.log('DATA BASE TYPE => REMOTE DB');
   dbName = process.env.DATABASE_NAME;
 } else {
-  console.log('DATA BASE TYPE => DEV DB');
-  DB = process.env.DATABASE_DEV;
-  // console.log('DATA BASE TYPE => LOCALE DB');
-  // DB = process.env.DATABASE_LOCALE;
+  // console.log('DATA BASE TYPE => DEV DB');
+  // DB = process.env.DATABASE_DEV;
+  
+  console.log('DATA BASE TYPE => LOCALE DB');
+  DB = process.env.DATABASE_LOCALE;
+
   dbName = process.env.DEV_DATABASE_NAME;
 }
 
